e5628b128de18846abc00bcd1b9b8aaceb4ca78f
Ryan Lavin <lavinrp@gmail.com>
2019-02-26 22:02:05 -0500

    git ignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/hdd/programming/cpp/Experiments/SFML_with_vcpkg
-# Build directory: /opt/hdd/programming/cpp/Experiments/SFML_with_vcpkg/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
